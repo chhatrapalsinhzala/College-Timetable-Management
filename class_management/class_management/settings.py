5bd746dc061685672bc545b4184f50d9a2a2d5e1
chhatrapal-digiqt <chhatrapal@digiqt.com>
2021-03-12 20:59:55 +0530

    all remaining files.

--- class_management/class_management/settings.py
+++ class_management/class_management/settings.py
@@ -73,12 +73,14 @@ WSGI_APPLICATION = 'class_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES={
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "HOST": "localhost", 
+        "USER": "postgres", 
+        "PASSWORD": "postgres",
+        "NAME": "ClassManagement", 
+        "PORT": "5432"}}
 
 
 # Password validation
